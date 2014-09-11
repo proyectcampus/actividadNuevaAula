52a7f4135a7751cf7ee09447d462e4585f6a43c0
miguel <mirope@campusaula.com>
2014-09-11 12:27:35 +0200

    añado commit3 issue54

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,12 @@
 public class HolaMundo{
 
 	public void Saludar(){
+
+		
 		System.out.print("hola");
 		System.out.print("hola1");
 		System.out.print("hola2");
+		System.out.print("hola3");
+	}
 
 }
